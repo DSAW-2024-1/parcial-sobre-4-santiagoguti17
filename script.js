f8ea429f7a5a6668c4159c0ccffe443c1b4cd47e
Santiago <santiagoguba@unisabana.edu.co>
2024-02-22 21:50:27 -0500

    pepepipe

---
+++ script.js
@@ -0,0 +1,10 @@
+function saveEmail() {
+    var emailInput = document.getElementById("email");
+    var email = emailInput.value;
+    // Do something with the email value
+    console.log(email);
+}
+var bottonGoIndex = document.getElementById("botton-sub")
+function goIndex(){
+    window.location.href = "index-succ.html"; 
+}
